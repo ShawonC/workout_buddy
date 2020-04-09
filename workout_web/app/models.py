7928fe6ae1af2e5715ac3e6d470b7b14f202d216
ShawonC <shawon@puppet.com>
2020-04-08 20:16:29 -0500

    added registration for the app

--- workout_web/app/models.py
+++ workout_web/app/models.py
@@ -1,12 +1,21 @@
 from datetime import datetime
 from app import db
+from app import login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    workouts = db.relationship('Workout', backref='date', lazy='dynamic')
+    workouts = db.relationship('Workout', backref='creator', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -17,7 +26,7 @@ class Workout(db.Model):
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     exercise_id = db.Column(db.Integer, db.ForeignKey('exercise.id'))
-    exercises = db.relationship('Exercise', backref='exercise_name', lazy='dynamic')
+    # exercises = db.relationship('Exercise', backref='exercise_name', lazy='dynamic')
 
     def __repr__(self):
         return f'<Workout {self.id}-{self.timestamp}>'
@@ -30,3 +39,7 @@ class Exercise(db.Model):
 
     def __repr__(self):
         return f'<Exercise {self.name}>'
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- workout_web/workout_web.py
+++ workout_web/workout_web.py
@@ -1,1 +1,11 @@
-from app import app
+from app import app, db
+from app.models import User, Workout, Exercise
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'User': User,
+        'Workout': Workout,
+        'Exercise': Exercise
+    }
